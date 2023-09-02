779f02d4d1eb5c6e592d2e0af561a934f10230ff
aadv1k <aadv1k@outlook.com>
2023-09-02 23:16:42 +0530

    Add initial failing test for the server

--- packages/server/tests/user.test.ts
+++ packages/server/tests/user.test.ts
@@ -1,17 +1,31 @@
 import request from 'supertest';
-import server from '../src/server';
+import app from '../src/server';
 
-import { describe, test } from '@jest/globals';
+import { describe, beforeAll, afterAll, test, expect } from '@jest/globals';
+
+const testUser = {
+    username: "john_doe",
+    email: "example@example.com",
+    password: "foo@123" 
+};
 
 describe('User Authentication Tests', () => {
-  test.todo('should create a new user');
+  test('should create a new user', async () => {
+    const response = await request(app)
+      .post('/v1/users')
+      .send(testUser);
+
+    const data = response.body;
+
+    expect(response.status).toBe(201);
+
+    // TODO: replace this more strongly typed instrinsics  
+    expect(data.status).toBe("success");
+  })
 
   test.todo('should not allow duplicate user creation');
-
   test.todo('should log in an existing user');
-
   test.todo('should reject an incorrect login');
   test.todo('should sign up a new user');
-
   test.todo('should handle signup failure gracefully');
 });
