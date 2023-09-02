bd96a32a87c67e19418c1b821b4641e9e32fd86b
aadv1k <aadv1k@outlook.com>
2023-09-02 21:46:59 +0530

    Respectfully, Yarn can go knit a sweater instead of JavaScript

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,1 +1,14 @@
-console.log("Hello world!");
+import dotenv from "dotenv"
+
+import express, { Request, Response } from "express";
+
+const PORT = 8080;
+const app = express();
+
+app.get("/", (req: Request, res: Response) => {
+    res.status(200).json({"Hello": "World"});
+});
+
+app.listen(PORT, () => {
+    console.log(`INFO: server listening on port ${PORT}`);
+});
